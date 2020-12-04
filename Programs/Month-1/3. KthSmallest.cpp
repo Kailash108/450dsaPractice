f3b18f70a55466c7b179c8c6751263b1993d5901
Kailash_mms <srikarkailash9@gmail.com>
2020-12-04 19:03:01 +0530

    Create 3. KthSmallest.cpp

---
+++ Programs/Month-1/3. KthSmallest.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i, size, tc, temp, j, pos;
+    cout << "Enter the no. of testcases: ";
+    cin >> tc;
+    while(tc--){
+        cout << "Enter the size of the array: ";
+        cin >> size;
+        int array[size];
+        cout << "Enter the elements into the array\n";
+        for(i=1;i<=size;i++){
+            cin >> array[i];
+        }   
+        for(i=1;i<=size;i++){
+            for(j=i+1;j<=size;j++){
+                if(array[i] > array[j]){
+                    temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;   
+                }
+            }
+        }
+        cout << "Enter the position: ";
+        cin >> pos;
+        cout << array[pos] << "\n";
+    }
+    return 0;
+}
