3cd62a6bbf583bc7d1967f15ea788c6024fa2306
Kailash_mms <srikarkailash9@gmail.com>
2020-12-11 14:31:08 +0530

    Update 18. rearrangeElements.py

--- Programs/Month-1/18. rearrangeElements.py
+++ Programs/Month-1/18. rearrangeElements.py
@@ -1,11 +1,9 @@
 # Link: https://www.geeksforgeeks.org/rearrange-array-alternating-positive-negative-items-o1-extra-space/
 # Single Line Input Is Not Possible In This Code
 size = int(input("Enter the size of the list: "))
-list = []
-final = []
+list = []; final = []
 print("Enter the elements into the list")
-n = 0
-p = 1
+n = 0 ; p = 1
 for i in range(0, size):
     ele = int(input())
     list.append(ele)
