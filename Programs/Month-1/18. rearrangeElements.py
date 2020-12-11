2c8a1c57a63937ba51a757533066d9bae97a8bad
Kailash_mms <srikarkailash9@gmail.com>
2020-12-11 14:26:55 +0530

    Create 18. rearrangeElements.py

---
+++ Programs/Month-1/18. rearrangeElements.py
@@ -0,0 +1,18 @@
+# Link: https://www.geeksforgeeks.org/rearrange-array-alternating-positive-negative-items-o1-extra-space/
+size = int(input("Enter the size of the list: "))
+list = []
+final = []
+print("Enter the elements into the list")
+n = 0
+p = 1
+for i in range(0, size):
+    ele = int(input())
+    list.append(ele)
+for i in range(0, size):
+    if(list[i] < 0):
+        final.insert(n, list[i])
+        n = n+2
+    elif(list[i] >= 0):
+        final.insert(p, list[i])
+        p = p+2
+print(final)
