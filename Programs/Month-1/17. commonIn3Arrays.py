8ffe0e9104258dcec86a47eb9afd235be1b3950a
Kailash_mms <srikarkailash9@gmail.com>
2020-12-11 12:44:15 +0530

    Create 17. commonIn3Arrays.py

---
+++ Programs/Month-1/17. commonIn3Arrays.py
@@ -0,0 +1,38 @@
+# Link: https://practice.geeksforgeeks.org/problems/common-elements1132/1
+n1 = int(input("Enter the size of array-1: "))
+n2 = int(input("Enter the size of array-2: "))
+n3 = int(input("Enter the size of array-3: "))
+
+A = [] 
+B = [] 
+C = []
+comAB = []
+finalList = []
+
+print("Enter the values into array-1")
+for a in range(0, n1):
+    ele = int(input())
+    A.append(ele)
+print("Enter the values into array-2")
+for b in range(0, n2):
+    ele = int(input())
+    B.append(ele)
+print("Enter the values into array-3")
+for c in range(0, n3):
+    ele = int(input())
+    C.append(ele)
+
+for i in range(0, n1):
+    for j in range(0, n2):
+        if(A[i] == B[j]):
+            comAB.append(A[i])
+            break
+for k in range(0, n3):
+    for m in range(0, len(comAB)):
+        if(C[k] == comAB[m]):
+            finalList.append(C[k])
+
+if(len(finalList) == 0):
+    print("No Common Elements Found In 3 Lists: ")
+else:
+    print("The Common Elements In Three Lists Are: "+finalList)
